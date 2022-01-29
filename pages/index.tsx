299fd6cd39bb2ab1beaa22a9ce0361ba2167e645
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-01-29 20:41:24 +0300

    prisma schema updated with names

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,55 @@
+import { Box, Text, Flex } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/react';
 import GradientLayout from '../components/gradientLayout';
+import { useMe } from '../lib/hooks';
 import prisma from '../lib/prisma';
-function Home() {
+
+const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
     <GradientLayout
-      color='green'
+      roundImage
+      color='gray'
       subtitle='profile'
-      title='Evan Bond'
-      description='7 public playlists'
-      image
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistsCount} public playlists`}
+      image='https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80'
     >
-      <div>home page</div>
+      <Box color='white' paddingX='40px'>
+        <Box marginBottom='40px'>
+          <Text fontSize='2xl' fontWeight='bold'>
+            Top artist this month
+          </Text>
+          <Text fontSize='md'>only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX='10px' width='20%'>
+              <Box bg='gray.900' borderRadius='4px' padding='15px' width='100%'>
+                <Image
+                  src='https://placekitten.com/300/300'
+                  borderRadius='100%'
+                />
+                <Box marginTop='20px'>
+                  <Text fontSize='large'>{artist.name}</Text>
+                  <Text fontSize='x-small'>Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
-}
-// export const getServerSideProps = async () => {
-//   const artists = await prisma.artist.findMany({});
+};
 
-//   return {
-//     props: { artists },
-//   };
-// };
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
+};
 
 export default Home;
