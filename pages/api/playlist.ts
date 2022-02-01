dd11c6a4c2bab3ad1ca34ca9b45c8fbf6b5c91d2
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-02-01 13:04:34 +0300

    natural sorting of playlists

--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -2,15 +2,20 @@ import prisma from '../../lib/prisma';
 import { validateRoute } from '../../lib/auth';
 
 export default validateRoute(async (req, res, user) => {
-  const playlists = await prisma.playlist.findMany({
-    where: {
-      userId: user.id,
-    },
+  // const playlists = await prisma.playlist.findMany({
+  //   where: {
+  //     userId: user.id,
+  //   },
 
-    orderBy: {
-      name: 'asc',
-    },
-  });
+  //   orderBy: {
+  //     name: 'asc',
+  //   },
+  // });
+
+  const playlists = await prisma.$queryRaw`
+  select * from "Playlist"
+  order by LENGTH(name), name
+  `;
 
   res.json(playlists);
 });
